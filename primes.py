64afc293983be6d1e5f18d9cf9fa0976e0b36d06
Yuxuan Zuo <146106195+3053887951@users.noreply.github.com>
2023-10-02 07:44:08 +0100

    Update primes.py
    finished

--- primes.py
+++ primes.py
@@ -1,6 +1,22 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError("Number of primes must be a positive integer")
+    num = 2
     list = []
-    return list
+    while len(list) < number_of_primes:
+        if is_prime(num):
+            list.append(num)
+        num += 1
+
+    return prime_list
